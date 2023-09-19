f578f08924c31f0c15b223c134faa994474782d0
samorag93 <moronyag90@gmail.com>
2023-09-19 15:47:48 +0200

    week2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Ahorramas S.A";
+        job1._jobTitle = "Auxiliar";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Cash Unide";
+        job2._jobTitle = "Cajero";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._company = "Carrefour";
+        job3._jobTitle = "Auxiliar";
+        job3._startYear = 2021;
+        job3._endYear = 2023;
+
+        Resume miResume = new Resume();
+        miResume._name = "Samuel Moroni Aguilar Cordova";
+
+        miResume._jobs.Add(job1);
+        miResume._jobs.Add(job2);
+        miResume._jobs.Add(job3);
+
+        miResume.Display();
     }
 }
