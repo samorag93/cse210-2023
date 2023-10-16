59fe3d8ca221998e8bf1054406bd8c701492cd83
samorag93 <moronyag90@gmail.com>
2023-10-16 22:22:11 +0200

    ok

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -9,19 +9,19 @@ class Program
 
         // Crear videos y comentarios
         Video _video1 = new Video("Tutorial de C#", "CSharpMaster123", 120);
-        _video1._comments.Add(new Comment("Usuario1", "Gran tutorial, ¡gracias!"));
-        _video1._comments.Add(new Comment("Usuario2", "No entendí la parte sobre bucles."));
-        _video1._comments.Add(new Comment("Usuario3", "Muy útil, ¡gracias!"));
+        _video1._comments.Add(new Comment("Usuario1", "Great tutorial, thanks!"));
+        _video1._comments.Add(new Comment("Usuario2", "I don't understand the loops"));
+        _video1._comments.Add(new Comment("Usuario3", "Very helpful, thanks!"));
 
-        Video _video2 = new Video("Receta de pastel de vainilla", "BakeQueen2000", 180);
-        _video2._comments.Add(new Comment("Usuario4", "Se ve delicioso, definitivamente lo intentaré."));
-        _video2._comments.Add(new Comment("Usuario5", "¿Se puede usar harina de trigo integral en lugar de harina normal?"));
-        _video2._comments.Add(new Comment("Usuario6", "¡Gracias por compartir!"));
+        Video _video2 = new Video("Vanilla Cake Recipe", "BakeQueen2000", 180);
+        _video2._comments.Add(new Comment("Usuario4", "It looks delicius, definitely I'll Try it"));
+        _video2._comments.Add(new Comment("Usuario5", "Can whole wheat flour be used instead of regular flour?"));
+        _video2._comments.Add(new Comment("Usuario6", "Thanks for sharing"));
 
         _videos.Add(_video1);
         _videos.Add(_video2);
         Console.Clear();
-        // Mostrar información de los videos y comentarios
+        // Display videos and comments
         foreach (var _video in _videos)
         {
             _video.Display();
